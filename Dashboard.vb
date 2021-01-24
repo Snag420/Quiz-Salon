f42a81f57af1898384afc99e841fdb75e5108234
Snag420 <77317373+Snag420@users.noreply.github.com>
2021-01-24 14:44:12 -0800

    Create Dashboard.vb

---
+++ Dashboard.vb
@@ -0,0 +1,22 @@
+Public Class Dashboard
+
+    Private Sub btnLayanan_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnLayanan.Click
+        frmLayanan.Show()
+    End Sub
+
+    Private Sub btnStylish_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnStylish.Click
+        frmStylish.Show()
+    End Sub
+
+    Private Sub btnPendaftaran_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnPendaftaran.Click
+        frmPendaftaran.Show()
+    End Sub
+
+    Private Sub btnPembayaran_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnPembayaran.Click
+        frmPembayaran.Show()
+    End Sub
+
+    Private Sub btnExit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnExit.Click
+        End
+    End Sub
+End Class
